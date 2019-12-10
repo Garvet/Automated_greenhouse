a1b547a564f1bf831c9341aecdb3317b855abef7
Artyom Kozyrev <39848982+q-him@users.noreply.github.com>
2019-12-10 13:26:01 +0300

    fixed weird compilation errors

--- lib/Hall_reader/Hall_reader.h
+++ lib/Hall_reader/Hall_reader.h
@@ -2,7 +2,13 @@
 #define GH_HALL_READER_H
 
 #include <Arduino.h>
-#include <Wire.h>
+
+// #include <Wire.h>
+/* Почему-то возникает ошибка компиляции
+ * C:\...\.platformio\lib\PCF8574_ESP_ID1659\pcf8574_esp.h:17:18: fatal error: Wire.h: No such file or directory
+ * но только если заголовок Hall_reader.h НЕ подключается в main.cpp
+ * если он подключается, то всё нормально, неважно поключён Wire.h или нет.
+ */
 #include <pcf8574_esp.h>
 
 // Класс для считывания с 10 датчиков Холла на двух расширителях PCF8574T
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include "Hall_reader.h"
 
 void setup() {
     
